79b4d085c429a0f279a74842da9fa68a0428d4c6
Aditi Jain <aditijain12390@gmail.com>
2022-06-23 13:00:13 +0530

    Header Completed..

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../components/Header/Header'
 
 export default function Home() {
   return (
@@ -7,7 +8,16 @@ export default function Home() {
         <title>Facebook</title>
       </Head>
 
-      <h1>Let's build Facebook</h1>
+      {/* Header */}
+      <Header />
+    
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </main>
+
+
 
     </div>
   )
